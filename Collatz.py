d3be162b14c3894dd8fe8f70b52a10459b307d87
Jeremiah Martinez <jeremiah@cs.utexas.edu>
2014-06-17 21:51:09 -0500

    Fixed the cache issue by making it global. Resolves #21

--- Collatz.py
+++ Collatz.py
@@ -9,6 +9,7 @@
 # ------------
 # collatz_read
 # ------------
+# import RunCollatz
 
 def collatz_read (r) :
     """
@@ -25,6 +26,7 @@ def collatz_read (r) :
 # ------------
 # collatz_eval
 # ------------
+array = [0] * 1000000
 
 def collatz_eval (i, j) :
     """
@@ -34,34 +36,46 @@ def collatz_eval (i, j) :
     """
     assert i, j > 0
     assert i, j < 1000000
-    array = [0] * 1000000
+
     cycle = 1
     max_cycle = 1
     x = 0
+
+    #swap values
     if (i > j) :
         a = j
         j = i
         i = a
+
+    # let m = j / 2. If i < m, max_cycle_length(i, j) = max_cycle_length(m, j)
     if (i < (j >> 1)) :
         i = j >> 1
     
+    # to be able to compute j itself
     j += 1
+
     for x in range(i, j) :
         current_Val = x
+
         if (array[x] != 0) :
             cycle = array[x]
         else :
             while current_Val > 1 :
+
                 if (current_Val % 2) == 0 :
                     current_Val = (current_Val >> 1)
                 else :
+                    #this does (3n + 1) AND divides by 2
                     current_Val =  current_Val + (current_Val >> 1) + 1
                     cycle += 1
+
                 cycle += 1
+
             array[x] = cycle
 
         if cycle > max_cycle :
             max_cycle = cycle
+
         cycle = 1
 
     assert max_cycle > 0
--- SphereCollatz.py
+++ SphereCollatz.py
@@ -39,6 +39,8 @@ def collatz_read (r) :
 # collatz_eval
 # ------------
 
+array = [0] * 1000000
+
 def collatz_eval (i, j) :
     """
     i is the beginning of the range, inclusive
@@ -47,38 +49,50 @@ def collatz_eval (i, j) :
     """
     assert i, j > 0
     assert i, j < 1000000
-    array = [0] * 1000000
+
     cycle = 1
     max_cycle = 1
     x = 0
+
+    #swap values
     if (i > j) :
         a = j
         j = i
         i = a
+
+    # let m = j / 2. If i < m, max_cycle_length(i, j) = max_cycle_length(m, j)
     if (i < (j >> 1)) :
         i = j >> 1
     
+    # to be able to compute j itself
     j += 1
+
     for x in range(i, j) :
         current_Val = x
+
         if (array[x] != 0) :
             cycle = array[x]
         else :
             while current_Val > 1 :
+
                 if (current_Val % 2) == 0 :
                     current_Val = (current_Val >> 1)
                 else :
+                    #this does (3n + 1) AND divides by 2
                     current_Val =  current_Val + (current_Val >> 1) + 1
                     cycle += 1
+
                 cycle += 1
+
             array[x] = cycle
 
         if cycle > max_cycle :
             max_cycle = cycle
+
         cycle = 1
 
     assert max_cycle > 0
-    return max_cycle  
+    return max_cycle   
 
 # -------------
 # collatz_print
